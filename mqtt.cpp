47a32521097be9882542cc2d1355ea348d70a2ee
hakidon <mohdanihakim@gmail.com>
2024-01-28 17:02:46 +0800

    Hotfix

--- mqtt.cpp
+++ mqtt.cpp
@@ -1,108 +1,65 @@
-/*
-ESP32 publish telemetry data to VOne Cloud and subcribe to relay
-*/
-
-#include "VOneMqttClient.h"
-
-//define device id
-const char* Relay = "f1aead0a-25ac-4007-ac76-5faef0dd8605";         //Replace with YOUR deviceID for relay
-
-//Used Pins
-const int relayPin = 10;       //Pin 14
-
-//Output
-
-//Create an instance of VOneMqttClient
-VOneMqttClient voneClient;
-
-//last message time
-unsigned long lastMsgTime = 0;
-
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+const char *WIFI_SSID = "xxx"; //your WiFi SSID
+const char *WIFI_PASSWORD = "xx"; // your password
+const char *MQTT_SERVER = "x.x.x.x"; // your VM instance public IP address
+const int MQTT_PORT = 1883;
+const char *MQTT_TOPIC = "iot"; // MQTT topic
+const int DHT_PIN = 38;
+const int DHT_TYPE = DHT11;
+DHT dht(DHT_PIN, DHT_TYPE);
+WiFiClient espClient;
+PubSubClient client(espClient);
+void setup_wifi()
+{
+ delay(10);
+ Serial.println("Connecting to WiFi...");
+ WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ while (WiFi.status() != WL_CONNECTED)
+ {
+ delay(500);
+ Serial.print(".");
+ }
+ Serial.println("WiFi connected");
+ Serial.println("IP address: ");
+ Serial.println(WiFi.localIP());
 }
-
-void triggerActuator_callback(const char* actuatorDeviceId, const char* actuatorCommand)
-{ 
-    //actuatorCommand format {"servo":90}
-    Serial.print("Main received callback : ");
-    Serial.print(actuatorDeviceId);
-    Serial.print(" : ");
-    Serial.println(actuatorCommand);
-    
-    String errorMsg = "";  
-
-    JSONVar commandObjct = JSON.parse(actuatorCommand);
-    JSONVar keys = commandObjct.keys();
-      
-    if(String(actuatorDeviceId) == Relay)
-    {
-       //{"LEDLight":false}
-        String key = "";
-        bool commandValue = "";
-        for (int i = 0; i < keys.length(); i++) {
-           key = (const char* )keys[i];
-           commandValue = (bool)commandObjct[keys[i]];
-          Serial.print("Key : ");
-          Serial.println(key.c_str());
-          Serial.print("value : ");
-          Serial.println(commandValue);           
-        }
-        
-        if(commandValue == true){ 
-           Serial.println("Relay ON");                
-           digitalWrite(relayPin, true);
-        }
-        else {
-          Serial.println("Relay OFF");    
-          digitalWrite(relayPin, false);
-        }   
-
-        voneClient.publishActuatorStatusEvent(actuatorDeviceId, actuatorCommand, true);
-        //Sample publish actuator fail status   
-        //errorMsg = "LED unable to light up.";
-        //voneClient.publishActuatorStatusEvent(actuatorDeviceId, actuatorCommand, errorMsg.c_str(), false);
-    }
+void setup()
+{
+ Serial.begin(115200);
+ dht.begin();
+ setup_wifi();
+ client.setServer(MQTT_SERVER, MQTT_PORT);
 }
-
-void setup() {  
-  
-  setup_wifi();   
-  voneClient.setup();
-  voneClient.registerActuatorCallback(triggerActuator_callback);
-  
-  //actuator 
-  pinMode(relayPin, OUTPUT);  
+void reconnect()
+{
+ while (!client.connected())
+ {
+ Serial.println("Attempting MQTT connection...");
+ if (client.connect("ESP32Client"))
+ {
+ Serial.println("Connected to MQTT server");
+ }
+ else
+ {
+ Serial.print("Failed, rc=");
+ Serial.print(client.state());
+ Serial.println(" Retrying in 5 seconds...");
+ delay(5000);
+ }
+ }
 }
-
-void loop() {
-
-  if (!voneClient.connected()) {
-    voneClient.reconnect();
-  }
-  voneClient.loop();
-
-  unsigned long cur = millis();
-  if (cur - lastMsgTime > INTERVAL) {
-    lastMsgTime = cur;
-       
-  }
+void loop()
+{
+ if (!client.connected())
+ {
+ reconnect();
+ }
+ client.loop();
+ delay(5000); // adjust the delay according to your requirements
+ float temperature = dht.readTemperature();
+ char payload[10];
+ sprintf(payload, "%.2f", temperature);
+ client.publish(MQTT_TOPIC, payload);
 }
